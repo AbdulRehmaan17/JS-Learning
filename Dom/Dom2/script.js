8b9e629556383f026fa44872232b8ea058df2c33
Abdul Rehman <abdulrehman396387@gmail.com>
2025-09-14 01:03:19 +0500

    Next Commit

---
+++ Dom/Dom2/script.js
@@ -0,0 +1,35 @@
+let heading = document.querySelector('h1');
+heading.innerText = "Hello I lOve Supra";
+heading.style.color = "red";
+console.log(heading.innerText);
+
+heading.innerText = "Hello I love toyota and mercedes";
+heading.style.color = "green";
+
+// let button = document.querySelectorAll('.btn1');
+// button.innerText = "oh yes Click me";
+// button.style.color = "blue";
+// button.style.backgroundColor = "yellow";
+// console.log(button);
+
+let button1 = document.getElementById("btn1");
+button1.innerText = "oh yes Click me";
+button1.style.color = "blue";
+button1.style.backgroundColor = "yellow";
+
+let button2 = document.getElementById("btn2");
+button2.innerText = "hey bay zara touch me";
+button2.style.color = "blue";
+button2.style.backgroundColor = "yellow";
+
+let button3 = document.getElementById("btn3");
+button3.innerText = "come on babe click me";
+button3.style.color = "blue";
+button3.style.backgroundColor = "yellow";
+
+let newDiv = document.getElementById("colorDiv");
+newDiv.style.backgroundColor = "orange";
+newDiv.style.border = "2px solid black";
+
+
+
